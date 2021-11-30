becc6b17b20b780e5e31ed1c7a920d6ae30b958d
AndresGB <a.galvisbolivar@gmail.com>
2021-11-30 01:07:55 -0500

    Login

--- project/rutas/rutasbd/usuario/user.py
+++ project/rutas/rutasbd/usuario/user.py
@@ -1,11 +1,59 @@
 from flask import Flask, render_template, redirect,url_for,request, flash
 from .. import mysql
 from .. import routes
+from .rol import *
+import bcrypt 
 
 #CRUD
-@routes.route('/usuario')
+@routes.route('/registrar')
 def usuario():
-    return render_template('./usuario/usuario.html')
+    return render_template('./usuario/usuario.html',roles = get_rol())
+
+@routes.route('/registrar_admin')
+def usuarioAdmin():
+    return render_template('./usuario/usuario.html',roles = get_rol())
+
+@routes.route('/login', METHODS=['GET'])
+def login():
+    try:
+        if request.method == 'GET':
+            username = request.form['username']
+            password = request.form['password']
+            hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT * FROM usuario WHERE username = %s", (username))
+            user = cur.fetchone()
+            print(user)
+            if user is None:
+                flash('Usuario no existe')
+                return redirect('/')
+            else:
+                if user.password == hashed:
+                    if user.rol == 'cliente':
+                        return redirect("./usuario/cliente")
+                    if user.rol == 'veterinario':
+                        return redirect("./usuario/veterinario")
+                    if user.rol == 'admin':
+                        return redirect("./usuario/admin")
+
+            
+
+            
+    except:
+        return redirect(url_for('rutas.login'))
+
+@routes.route('/user/<string:id>', methods = ['GET'])
+def get_user(id):
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM usuario WHERE username = %s', (id))
+        data = cur.fetchall()
+        cur.close()
+        flash('Usuario encontrado')
+        return render_template('./usuario/usuario.html',usuario=data[0])
+    except:
+        flash('Error al recuperar el usuario')
+        return redirect(url_for('usuario'))
 
 #Create
 @routes.route('/add_user', methods=['POST'])
@@ -29,9 +77,9 @@ def add_user():
 
 #edit
 @routes.route('/edit_user/<string:id>', methods = ['POST', 'GET'])
-def get_contact(id):
+def get_contact_edit(id):
     cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM usuario WHERE id = %s', (id))
+    cur.execute('SELECT * FROM usuario WHERE username = %s', (id))
     data = cur.fetchall()
     cur.close()
     return render_template('edit-user.html', contact = data[0])
