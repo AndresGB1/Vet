39798f8524029701b9bacff9929b45b2130c34f2
AndresGB <a.galvisbolivar@gmail.com>
2021-12-10 21:21:24 -0500

    intento

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from rutas import routes
+from flask_mysqldb import MySQL
+import os
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello world'
+app =  Flask(__name__)
+#Conexión a BD con archivo .env
+app.config['MYSQL_USER'] = os.environ.get('USERDB') #'usuario'
+app.config['MYSQL_PASSWORD'] = os.getenv('PASSWORDDB') #'password'
+app.config['MYSQL_DB'] = os.getenv('DB') #'Base de datos'
+app.config['MYSQL_HOST'] = os.getenv('HOSTDB') #'localhost'
+app.secret_key = 'mysecretkey'
+
+mysql=MySQL(app)
+app.register_blueprint(routes) #Registrar las rutas
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(port = 5000, debug = False)
--- rutas/__init__.py
+++ rutas/__init__.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 
 routes = Blueprint('routes', __name__,template_folder='templates')
 
-from App import mysql
+from main import mysql
 from .index import *
 from .rutasbd.__init__ import *
